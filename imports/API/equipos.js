0166c14738495e2e6fe144a5462ed36f6b23a05e
wa-p montoya <wa_p@hotmail.com>
2018-07-08 20:14:41 -0400

    First Deploy

--- imports/API/equipos.js
+++ imports/API/equipos.js
@@ -21,5 +21,46 @@ if (Meteor.isServer) {
     return Meteor.users.find({'emails.address':{$nin:['antonio@ucv.ve']}});
   });
 
-}
 
+ Meteor.publish('getPrice', function() {
+    var self = this;
+    var init = true;
+    /*
+    try {
+      var res = HTTP.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD', {});
+
+      var item = JSON.parse(res.content);
+      
+     
+	
+      self.added('price', 1, {USD: item.BTC.USD,NAME:'BTC'});
+      self.added('price', 2, {USD: item.LTC.USD,NAME:'LTC'});
+      self.added('price', 3, {USD: item.ETH.USD,NAME:'ETH'});
+		
+      self.ready();
+    } catch(error) {
+      console.log(error);
+    }
+
+*/
+
+
+		Meteor.setInterval(function() {
+		    var res = HTTP.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD', {});
+		    var item = JSON.parse(res.content);
+		    if (init) {
+		      self.added('price', 1, {USD: item.BTC.USD,NAME:'BTC'});
+		      self.added('price', 2, {USD: item.LTC.USD,NAME:'LTC'});
+		      self.added('price', 3, {USD: item.ETH.USD,NAME:'ETH'});
+		    } else {
+		      self.changed('price', 1, {USD: item.BTC.USD,NAME:'BTC'});
+		      self.changed('price', 2, {USD: item.LTC.USD,NAME:'LTC'});
+		      self.changed('price', 3, {USD: item.ETH.USD,NAME:'ETH'});
+		    }
+		    self.ready();
+		    init = false;
+		  }, 10000);
+
+});
+
+}
--- imports/body.js
+++ imports/body.js
@@ -2,31 +2,51 @@ import { Template } from 'meteor/templating';
 import { Equipos } from './API/equipos.js';
 import { Super } from './API/equipos.js';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { Session } from 'meteor/session'
 
  
 
 import './body.html';
 
+  Price = new Mongo.Collection('price');
+
+ 
+
+ 
  
 Template.body.onCreated(function bodyOnCreated() {
 
   this.state = new ReactiveDict();
-  this.state.set('email_user':'pepe@gmail.com');
+  Session.set('email_user',null)
+  
 
   Meteor.subscribe('equipos');
   Meteor.subscribe('super');
   Meteor.subscribe('user');
+  Meteor.subscribe('getPrice');
 });
 
 
 
 Template.body.helpers({
+
+	 prices() {
+      return Price.find();
+
+    },
 	
 
   equipos() {
   		
 			if (Super.find({user:Meteor.user().emails[0].address}).count() == 1 ){
-				return Equipos.find();
+					if(Session.get('email_user')==null){
+						return Equipos.find();
+					}else{
+						return Equipos.find({'owner':Session.get('email_user')});
+					}
+
+					
+				
 			}else{
 				return Equipos.find({
 					 owner: Meteor.user().emails[0].address	
@@ -39,25 +59,45 @@ Template.body.helpers({
   				return Meteor.users.find({'emails.address':{$nin:['antonio@ucv.ve']}});
   		}
   		
-  },
-
-  usuario: function () {
-      return Template.instance().state.get('email_user');
-    }
+  }
 
 });
 
-Template.user.created = function () {
-    // counter starts at 0
-    this.state = new ReactiveDict();
-    this.state.set('email_user', 'pepe@gmail.com');
-  };
+Template.body.events({
+'click .js-user':function(event,temp){
+	//var a = event.target.innerHTML;
+	//alert(a);
+	//console.log(event.target.getAttribute("data-valor"));
+	//console.log(temp.state.get( 'email_user' ));
+	var a = event.target.getAttribute("data-valor");
+	if (a == null){
+		Session.set('email_user',null);
+	}
+	console.log(event.target.getAttribute("data-valor"))
+	
+
+
+}
+
+});
 
 
 Template.user.events({
 'click .js-user':function(event,temp){
-	//alert(event.target.innerHTML);
-	console.log(temp.state.get( 'email_user' ));
+	//var a = event.target.innerHTML;
+	//alert(a);
+	//console.log(event.target.getAttribute("data-valor"));
+	//console.log(temp.state.get( 'email_user' ));
+	var a = event.target.getAttribute("data-valor");
+	if (a == "null"){
+		Session.set('email_user',null);
+	}else{
+		Session.set('email_user',a);
+	
+	}
+	console.log(event.target.getAttribute("data-valor"))
+	
+
 
 }
 
